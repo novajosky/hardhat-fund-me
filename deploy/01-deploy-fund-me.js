ccc8853110023542252aea19299a11399a19ce27
Michael Novajosky <mikenovajosky@yahoo.com>
2022-08-08 16:57:59 -0700

    first commit

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,11 @@
+const { getNamedAccounts, deployments, network } = require("hardhat")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    
+}
