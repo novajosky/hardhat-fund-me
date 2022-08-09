bdea6d4b025bf35cef30359171c03b38b13cfedc
Michael Novajosky <mikenovajosky@yahoo.com>
2022-08-09 09:53:49 -0700

    refactor for pricefeed address dependant on network

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -30,11 +30,6 @@ contract FundMe {
         funders.push(msg.sender);
     }
     
-    function getVersion() public view returns (uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-        return priceFeed.version();
-    }
-    
     modifier onlyOwner {
         // require(msg.sender == owner);
         if (msg.sender != owner) revert NotOwner();
