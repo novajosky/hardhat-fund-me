2ac859ffe3600e4d1241193bccb3ca6dcda3e541
Michael Novajosky <mikenovajosky@yahoo.com>
2022-08-19 15:34:14 -0700

    deploy balance test with multiple funders

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -75,5 +75,46 @@ describe("Fund Me", async function () {
                 endingDeployerBalance.add(gasCost).toString()
             )
         })
+        it("allows us to withdraw with multiple funders", async function (){
+            const accounts = await ethers.getSigners()
+            for (let i = 1; i < 6; i++) {
+                const fundMeConnectedContract = await fundMe.connect(
+                    accounts[i]
+                )
+                await fundMeConnectedContract.fund({ value: sendValue })
+            }
+            const startingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            )
+            const startingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            )
+            // Act
+            const transactionResponse = await fundMe.withdraw()
+            const transactionReceipt = await transactionResponse.wait(1)
+            const { gasUsed, effectiveGasPrice } = transactionReceipt
+            const gasCost = gasUsed.mul(effectiveGasPrice)
+
+            const endingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            )
+            const endingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            )
+
+            // Assert
+            assert.equal(endingFundMeBalance, 0)
+            assert.equal(
+                startingFundMeBalance.add(startingDeployerBalance).toString(),
+                endingDeployerBalance.add(gasCost).toString()
+            )
+
+            //Make sure that the funders are reset properly
+            await expect(fundMe.funders(0)).to.be.reverted
+            
+            for (i = 1; i < 6; i++){
+                assert.equal(await fundMe.addressToAmountFunded(accounts[i].address), 0)
+            }
+        })
     })
 })
